31856a13a828e0b805a86517806d36bf46c2f1fd
Dave Rodrigues <dr72@buffalo.edu>
2020-02-26 00:35:50 -0500

    Added Floating button to add appointment to schedule and create schedule activity. #37 #45

---
+++ 368/app/src/main/java/com/example/a368/ui/today/AddToSchedule.java
@@ -0,0 +1,30 @@
+package com.example.a368.ui.today;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.a368.R;
+
+public class AddToSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_schedule);
+
+        Button btCreate = findViewById(R.id.btCreateAppointment);
+
+        btCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddToSchedule.this, "Appointment Created", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+    }
+}
--- 368/app/src/main/java/com/example/a368/ui/today/TodayFragment.java
+++ 368/app/src/main/java/com/example/a368/ui/today/TodayFragment.java
@@ -1,5 +1,6 @@
 package com.example.a368.ui.today;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.a368.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Date;
 
@@ -25,6 +27,15 @@ public class TodayFragment extends Fragment {
         View root = inflater.inflate(R.layout.today_schedule, container, false);
 
         TextView remaining_time = (TextView)root.findViewById(R.id.remaining_time_textview);
+        FloatingActionButton fab = (FloatingActionButton)root.findViewById(R.id.fabAddAppointment);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), AddToSchedule.class));
+            }
+        });
+
 
         String currentDateTimeString = java.text.DateFormat.getDateTimeInstance().format(new Date());
         remaining_time.setText(currentDateTimeString);
