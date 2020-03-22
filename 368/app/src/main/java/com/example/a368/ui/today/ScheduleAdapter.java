9b6ef78ff869e18f73d07a2e65828eec3479c9e4
Dave Rodrigues <dr72@buffalo.edu>
2020-03-22 16:12:35 -0400

    #25 My Schedule -Time Remaining functionality added, today schedule displays time remaining before appointment

--- 368/app/src/main/java/com/example/a368/ui/today/ScheduleAdapter.java
+++ 368/app/src/main/java/com/example/a368/ui/today/ScheduleAdapter.java
@@ -10,7 +10,12 @@ import android.widget.TextView;
 import com.example.a368.R;
 import com.example.a368.Schedule;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /*
 Created by: Jiwon Choi
@@ -44,6 +49,36 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
         holder.text_end_date.setText(schedule.getEnd_date());
         holder.text_end_time.setText(String.valueOf(schedule.getEnd_time()));
         holder.text_description.setText(String.valueOf(schedule.getDescription()));
+
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("MMM dd yyyy'T'hh:mm aa");
+        String s = schedule.getStart_date()+" "+Calendar.getInstance().get(Calendar.YEAR)+"T"+schedule.getStart_time();
+        Date date = null;
+        try {
+            date = format.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long remaining = date.getTime() - currentTime.getTime();
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = remaining / daysInMilli;
+        remaining = remaining % daysInMilli;
+
+        long elapsedHours = remaining / hoursInMilli;
+        remaining = remaining % hoursInMilli;
+
+        long elapsedMinutes = remaining / minutesInMilli;
+        remaining = remaining % minutesInMilli;
+
+        long elapsedSeconds = remaining / secondsInMilli;
+        String formatted = String.format("%d:%02d:%02d", elapsedHours, elapsedMinutes, elapsedSeconds);
+        holder.text_time_remaining.setText("Remains\n"+formatted);
+
     }
 
     @Override
@@ -53,7 +88,7 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public TextView text_name, text_start_date, text_start_time, text_end_date, text_end_time, text_description;
+        public TextView text_name, text_start_date, text_start_time, text_end_date, text_end_time, text_description, text_time_remaining;
         onClickListener onClickListener;
 
         public ViewHolder(View itemView, onClickListener onClickListener) {
@@ -65,7 +100,7 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
             text_end_date = itemView.findViewById(R.id.text_end_date);
             text_end_time = itemView.findViewById(R.id.text_end_time);
             text_description = itemView.findViewById(R.id.text_description);
-
+            text_time_remaining = itemView.findViewById(R.id.time_remaining);
             this.onClickListener = onClickListener;
             itemView.setOnClickListener(this);
         }
