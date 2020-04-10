414b595fa838a470b927796cf0fcfd99e97667c5
Jiwon Choi <jiwoncho@buffalo.edu>
2020-04-10 04:30:24 -0400

    filtered out people that are already added as friend in Add Friend window; margin between search bar and registered user list is modified

--- 368/app/src/main/java/com/example/a368/ui/friends/AddFriendActivity.java
+++ 368/app/src/main/java/com/example/a368/ui/friends/AddFriendActivity.java
@@ -42,8 +42,9 @@ import java.util.List;
 public class AddFriendActivity extends AppCompatActivity implements FriendSearchAdapter.onClickListener {
 
     private static String url = "https://www-student.cse.buffalo.edu/CSE442-542/2020-spring/cse-442w/user/fetch_user.php";
+    private static String url_friend = "https://www-student.cse.buffalo.edu/CSE442-542/2020-spring/cse-442w/friend/fetch_friend.php";
     private RecyclerView uList;
-//    private RecyclerView.Adapter adapter;
+    private ArrayList<String> friendList;
     private ArrayList<Friend> userList;
     FriendSearchAdapter mAdapter;
     SearchView searchView;
@@ -83,10 +84,12 @@ public class AddFriendActivity extends AppCompatActivity implements FriendSearch
             }
         });
 
+        friendList = new ArrayList<>();
+        getFriend();
+
         uList = (RecyclerView) findViewById(R.id.friendsRecyclerView);
         userList = new ArrayList<>();
         mAdapter = new FriendSearchAdapter(this, userList, this);
-
         linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         dividerItemDecoration = new DividerItemDecoration(uList.getContext(), linearLayoutManager.getOrientation());
@@ -105,7 +108,7 @@ public class AddFriendActivity extends AppCompatActivity implements FriendSearch
         return super.onSupportNavigateUp();
     }
 
-    // On click schedule item
+    // On click to add friend
     @Override
     public void onClickFriend(int position) {
 
@@ -118,7 +121,45 @@ public class AddFriendActivity extends AppCompatActivity implements FriendSearch
         getData();
     }
 
-    // Fetch JSON data to display schedule
+    // Fetch JSON data to display existing friend list
+    private void getFriend() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url_friend, new Response.Listener<JSONArray>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onResponse(JSONArray response) {
+                friendList.clear();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        if (jsonObject.getString("email_a").equals(User.getInstance().getEmail())) {
+                            friendList.add(jsonObject.getString("email_b"));
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        progressDialog.dismiss();
+                    }
+                }
+
+                mAdapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    // Fetch JSON data to display registered user list
     private void getData() {
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading...");
@@ -134,7 +175,8 @@ public class AddFriendActivity extends AppCompatActivity implements FriendSearch
                         JSONObject jsonObject = response.getJSONObject(i);
 
                         // check to display the logged-in user's schedule only && display today's schedule only
-                        if (!(jsonObject.getString("email").equals(User.getInstance().getEmail()))) {
+                        if (!(jsonObject.getString("email").equals(User.getInstance().getEmail())) &&
+                            !(friendList.contains(jsonObject.getString("email")))) {
 
                             Friend friend = new Friend();
                             friend.setName(jsonObject.getString("name"));
