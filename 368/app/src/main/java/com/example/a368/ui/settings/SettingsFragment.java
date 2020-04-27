c08d559182eb84b5d533d19914a8cb3f9aa880d6
Jiwon Choi <jiwoncho@buffalo.edu>
2020-04-27 02:00:49 -0400

    #79 logout feature successfully migrated from the applicaiton toolbar, located on top right corner (indicated with three dots)

--- 368/app/src/main/java/com/example/a368/ui/settings/SettingsFragment.java
+++ 368/app/src/main/java/com/example/a368/ui/settings/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.example.a368.ui.settings;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,22 +11,36 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.a368.R;
+import com.example.a368.User;
+import com.example.a368.ui.login.LoginActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     private SettingsViewModel settingsViewModel;
     private EditText oldPassword;
     private EditText newPassword;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        pref = getActivity().getSharedPreferences("MyPref", 0); // 0 - for private mode
+        editor = pref.edit();
 
         Preference preference = findPreference("logout");
         preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                Toast.makeText(getContext(), "logout clicked", Toast.LENGTH_SHORT).show();
+                editor.putString("User", null);
+                editor.commit();
+                User user = User.getInstance();
+                user.setEmail("");
+                user.setName("");
+                Intent intent = new Intent(SettingsFragment.this.getContext(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+//                Toast.makeText(getContext(), "logout clicked", Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
