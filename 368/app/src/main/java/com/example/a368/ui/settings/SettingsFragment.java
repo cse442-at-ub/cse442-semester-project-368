6fc1af5f421c952bdbfd95447eb1e6813f0d7b70
Jiwon Choi <jiwoncho@buffalo.edu>
2020-04-28 02:02:18 -0400

    #79 enhanced UX by providing alert dialog to re-ask about loggin out from the application, in order to avoid accidentally clicking 'logout' menu

--- 368/app/src/main/java/com/example/a368/ui/settings/SettingsFragment.java
+++ 368/app/src/main/java/com/example/a368/ui/settings/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.example.a368.ui.settings;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -32,15 +34,30 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                editor.putString("User", null);
-                editor.commit();
-                User user = User.getInstance();
-                user.setEmail("");
-                user.setName("");
-                Intent intent = new Intent(SettingsFragment.this.getContext(), LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-//                Toast.makeText(getContext(), "logout clicked", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(getContext());
+                confirmBuilder.setTitle("Add Friend");
+                confirmBuilder.setMessage("Are you sure you want to log out?");
+                confirmBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        editor.putString("User", null);
+                        editor.commit();
+                        User user = User.getInstance();
+                        user.setEmail("");
+                        user.setName("");
+                        Intent intent = new Intent(SettingsFragment.this.getContext(), LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                });
+                confirmBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                confirmBuilder.show();
+
                 return true;
             }
         });
