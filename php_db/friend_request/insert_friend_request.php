4fa093dae45e93377f4b221ad02499e8b44f58f6
Jiwon Choi <jiwoncho@buffalo.edu>
2020-05-02 23:26:59 -0400

    #110 error fixed for variables in the inserting friend request php file

--- php_db/friend_request/insert_friend_request.php
+++ php_db/friend_request/insert_friend_request.php
@@ -4,11 +4,11 @@ include 'DatabaseConfig.php';
 
  $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 
- $email_a=$_POST['sender_name'];
- $email_a=$_POST['sender_email'];
- $email_b=$_POST['receiver_name'];
- $email_b=$_POST['receiver_email'];
- $email=$_POST['status'];
+ $sender_name=$_POST['sender_name'];
+ $sender_email=$_POST['sender_email'];
+ $receiver_name=$_POST['receiver_name'];
+ $receiver_email=$_POST['receiver_email'];
+ $status=$_POST['status'];
 
  $Sql_Query = "insert into friend_request_data (sender_name,sender_email,receiver_name,receiver_email,status) values ('$sender_name','$sender_email','$receiver_name','$receiver_email','$status')";
  
@@ -18,7 +18,6 @@ include 'DatabaseConfig.php';
  elseif (empty($sender_name)) {
  echo "Data cannot be inserted directly through html.";
  } 
-
 else {
  echo 'Try Again';
  }
