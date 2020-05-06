c0b457d88d8a4a1caa30ad69142a40ded770db8e
Jiwon Choi <jiwoncho@buffalo.edu>
2020-05-06 01:00:20 -0400

    #116 color-coded status button (pending and confirm) enabled to show the current status; the user can cancel the request that he sent and/or the request that he already confirmed but his groupmate(s) have not confirmed yet

--- 368/app/src/main/java/com/example/a368/ui/appointment_meeting/MeetingRequestAdapter.java
+++ 368/app/src/main/java/com/example/a368/ui/appointment_meeting/MeetingRequestAdapter.java
@@ -1,9 +1,11 @@
 package com.example.a368.ui.appointment_meeting;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -37,11 +39,30 @@ public class MeetingRequestAdapter extends RecyclerView.Adapter<MeetingRequestAd
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.tvMeetingTitle.setText(list.get(position).getTitle());
         holder.tvMeetingTime.setText(String.format("%s %s - %s %s", list.get(position).getStart_date(), list.get(position).getStart_time(), list.get(position).getEnd_date(), list.get(position).getEnd_time()));
+        holder.btnStatus.setText(list.get(position).getStatus());
 
         if (list.get(position).getDescription().equals("")) {
             holder.tvMeetingReqDescription.setVisibility(View.GONE);
         } else {
             holder.tvMeetingReqDescription.setText(list.get(position).getDescription());
+        }
+
+        // Set button text differently based on the current request status
+        if (list.get(position).getStatus().equals("accepted")) {
+            holder.btnStatus.getBackground().setColorFilter(holder.btnStatus.getContext().getResources().
+                    getColor(R.color.colorStatusGreen), PorterDuff.Mode.MULTIPLY);
+            holder.btnStatus.setEnabled(false);
+        } else if (list.get(position).getStatus().equals("pending")) {
+            holder.btnStatus.getBackground().setColorFilter(holder.btnStatus.getContext().getResources().
+                    getColor(R.color.colorStatusYellow), PorterDuff.Mode.MULTIPLY);
+            holder.btnStatus.setEnabled(false);
+        } else if (list.get(position).getStatus().equals("confirm")) {
+            holder.btnStatus.getBackground().setColorFilter(holder.btnStatus.getContext().getResources().
+                    getColor(R.color.colorStatusBlue), PorterDuff.Mode.MULTIPLY);
+        } else if (list.get(position).getStatus().equals("rejected")) {
+            holder.btnStatus.getBackground().setColorFilter(holder.btnStatus.getContext().getResources().
+                    getColor(R.color.colorStatusRed), PorterDuff.Mode.MULTIPLY);
+            holder.btnStatus.setEnabled(false);
         }
 
     }
@@ -50,10 +71,12 @@ public class MeetingRequestAdapter extends RecyclerView.Adapter<MeetingRequestAd
     public int getItemCount() {
         return list.size();
     }
+
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tvMeetingTitle;
         TextView tvMeetingTime;
         TextView tvMeetingReqDescription;
+        Button btnStatus;
         LinearLayout layout;
         onClickListener onClickListener;
 
@@ -63,10 +86,13 @@ public class MeetingRequestAdapter extends RecyclerView.Adapter<MeetingRequestAd
             tvMeetingTitle = itemView.findViewById(R.id.tvMeetingReqTitle);
             tvMeetingTime = itemView.findViewById(R.id.tvMeetingReqTime);
             tvMeetingReqDescription = itemView.findViewById(R.id.tvMeetingReqDescription);
+            btnStatus = itemView.findViewById(R.id.meeting_status);
             layout = itemView.findViewById(R.id.meetingReqLayout);
             this.onClickListener = onClickListener;
             itemView.setOnClickListener(this);
+
         }
+
         @Override
         public void onClick(View v) {
             onClickListener.onClickRequest(getAdapterPosition());
